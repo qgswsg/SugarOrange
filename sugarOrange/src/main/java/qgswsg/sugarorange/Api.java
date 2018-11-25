28802bd3735987d4a0836a17b37093d8074c6469
qgswsg <qgswsg@msn.cn>
2018-11-25 23:42:48 +0800

    未完成

--- sugarOrange/src/main/java/qgswsg/sugarorange/Api.java
+++
@@ -1,9 +0,0 @@
-package qgswsg.sugarorange;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
-public @interface Api {
-    String value();
-}
