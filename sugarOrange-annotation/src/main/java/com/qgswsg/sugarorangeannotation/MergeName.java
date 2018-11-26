0d4e1b66f838ac89d8b44b3ea3dd49001cf2f61d
qgswsg <qgswsg@msn.cn>
2018-11-26 22:17:35 +0800

    删除有问题的文件

--- sugarOrange-annotation/src/main/java/com/qgswsg/sugarorangeannotation/MergeName.java
+++
@@ -1,9 +0,0 @@
-package com.qgswsg.sugarorangeannotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.SOURCE)
-public @interface MergeName {
-    String value();
-}
