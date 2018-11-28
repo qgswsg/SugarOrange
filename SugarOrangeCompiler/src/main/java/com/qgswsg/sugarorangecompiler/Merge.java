69246f2af46447d83937d01fc7c11de074bb7986
qgswsg <qgswsg@msn.cn>
2018-11-28 23:12:21 +0800

    完成URL合成功能

--- SugarOrangeCompiler/src/main/java/com/qgswsg/sugarorangecompiler/Merge.java
+++ SugarOrangeCompiler/src/main/java/com/qgswsg/sugarorangecompiler/Merge.java
@@ -4,7 +4,6 @@ import com.google.auto.common.SuperficialValidation;
 import com.qgswsg.sugarorangeannotation.Api;
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
@@ -15,11 +14,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
@@ -32,8 +33,13 @@ import javax.tools.Diagnostic;
 
 class Merge {
 
+    private static final String VALUE = "value";
+    private static final String STRING = "java.lang.String";
+    private static final String HTTP = "http";
+    private static final String[] objMethod = new String[]{"getClass", "hashCode", "equals", "toString", "notify", "notifyAll", "wait"};
+    private static final String[] httpMethod = new String[]{"GET", "POST", "OPTIONS", "DELETE", "PUT", "HEAD", "PATCH"};
+
     private ProcessingEnvironment env;
-    private String[] objMethod = new String[]{"getClass", "hashCode", "equals", "toString", "notify", "notifyAll", "wait"};
 
     Merge(ProcessingEnvironment env) {
         this.env = env;
@@ -48,26 +54,26 @@ class Merge {
         for (Element element : elements) {
             if (!SuperficialValidation.validateElement(element)) continue;
             PackageElement packageElement = env.getElementUtils().getPackageOf(element);
-            String baseUrl = element.getAnnotation(Api.class).baseUrl();
+            String baseUrl = element.getAnnotation(Api.class).value();
             pkName = packageElement.getQualifiedName().toString();
             for (Element membersElement : env.getElementUtils().getAllMembers((TypeElement) element)) {
                 if (membersElement.getKind().equals(ElementKind.METHOD)) {
                     ExecutableElement executableElement = (ExecutableElement) membersElement;
                     if (Arrays.asList(objMethod).contains(executableElement.getSimpleName().toString()))
                         continue;
+                    String docComment = env.getElementUtils().getDocComment(executableElement);
                     MethodSpec methodSpec = MethodSpec.methodBuilder(executableElement.getSimpleName().toString())
                             .addModifiers(executableElement.getModifiers())
                             .returns(ClassName.get(executableElement.getReturnType()))
                             .addParameters(getParameter(executableElement))
                             .addAnnotations(getElementAnnotationMirrors(membersElement, baseUrl))
-                            .addJavadoc(env.getElementUtils().getDocComment(executableElement)).build();
+                            .addJavadoc(docComment == null ? "" : docComment).build();
                     methodSpecs.add(methodSpec);
                 }
             }
         }
         mergeApiInterfaceBuilder.addMethods(methodSpecs);
         JavaFile javaFile = JavaFile.builder(pkName, mergeApiInterfaceBuilder.build()).build();
-        note(null, mergeApiInterfaceBuilder.build().toString());
         try {
             javaFile.writeTo(env.getFiler());
         } catch (IOException e) {
@@ -100,10 +106,22 @@ class Merge {
         List<AnnotationSpec> annotationSpecList = new ArrayList<>();
         for (AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
             AnnotationSpec annotationSpec = AnnotationSpec.get(annotationMirror);
-            if (annotationSpec.members.size() == 1 && annotationSpec.members.containsKey("value")) {
-                // @Named("foo")
-                List<CodeBlock> value = annotationSpec.members.get("value");
-                annotationSpec = AnnotationSpec.builder(ClassName.get("retrofit2.http", "GET")).addMember("value", CodeBlock.builder().add("\"$L\" + $L", baseUrl, value.get(0).toString()).build()).build();
+            if (Arrays.asList(httpMethod).contains(annotationMirror.getAnnotationType().asElement().getSimpleName().toString())) {
+                AnnotationSpec.Builder builder = AnnotationSpec.builder(ClassName.bestGuess(annotationMirror.getAnnotationType().toString()));
+                if (!annotationMirror.getElementValues().isEmpty()) {
+                    for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> next : annotationMirror.getElementValues().entrySet()) {
+                        String url = null;
+                        if (next.getKey().getSimpleName().toString().equals(VALUE) && next.getKey().getReturnType().toString().equals(STRING)) {
+                            if (!next.getValue().getValue().toString().toLowerCase().startsWith(HTTP)) {
+                                url = baseUrl + next.getValue().getValue().toString();
+                            }
+                        }
+                        builder.addMember(VALUE, url == null ? "$L" : "$S", url == null ? next.getValue() : url);
+                    }
+                } else {
+                    builder.addMember(VALUE, "$S", baseUrl);
+                }
+                annotationSpec = builder.build();
             }
             annotationSpecList.add(annotationSpec);
         }
