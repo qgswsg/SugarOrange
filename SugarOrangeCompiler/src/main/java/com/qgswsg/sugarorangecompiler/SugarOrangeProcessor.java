55c4699e05d01084b7cf9ab5f42251cc9ef6fc85
qgswsg <qgswsg@msn.cn>
2019-03-28 18:13:14 +0800

    Update SugarOrangeProcessor.java
    添加注释

--- SugarOrangeCompiler/src/main/java/com/qgswsg/sugarorangecompiler/SugarOrangeProcessor.java
+++ SugarOrangeCompiler/src/main/java/com/qgswsg/sugarorangecompiler/SugarOrangeProcessor.java
@@ -43,18 +43,20 @@ public class SugarOrangeProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
         if (merge == null) {
+            //由于这个方法会被回调两次，这里进行一次判断，只执行一次合并逻辑
             merge = new Merge(processingEnv);
             Set<? extends Element> mergeNameElements = roundEnvironment.getElementsAnnotatedWith(MergeName.class);
-            if (mergeNameElements.size() > 1) {
+            if (mergeNameElements.size() > 1) {//如果@MergeName注解存在多个，就在构建项目时报错并提示
                 Element next = mergeNameElements.iterator().next();
                 error(next, "Cannot have multiple @MergeName annotations at the same time");
             } else {
                 if (mergeNameElements.size() == 1) {
+                    //如果项目中有且仅有一个@MergeName注解时，取此注解中的值作合并后的接口的文件名
                     String value = mergeNameElements.iterator().next().getAnnotation(MergeName.class).value();
                     if (!value.isEmpty()) {
                         merge.start(value, roundEnvironment);
                     }
-                } else {
+                } else {//如果项目中未使用@MergeName指定合并后的名字，就使用"SugarOrangeApiService"这个默认名字
                     merge.start("SugarOrangeApiService", roundEnvironment);
                 }
             }
@@ -62,10 +64,16 @@ public class SugarOrangeProcessor extends AbstractProcessor {
         return false;
     }
 
+    /**
+    * 在构建信息中打印错误消息
+    **/
     private void error(Element element, String message, Object... args) {
         printMessage(Diagnostic.Kind.ERROR, element, message, args);
     }
 
+    /**
+    * 在构建信息中打印普通信息
+    **/
     private void note(Element element, String message, Object... args) {
         printMessage(Diagnostic.Kind.NOTE, element, message, args);
     }
